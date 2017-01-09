cf4f74e9e257e6622aaa83ac783336e466b17c5a
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-01-09 22:01:10 +0100

    Update index.js with readFile() and readDir

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
 	stat : require('./stat.js'),
 	mkDir : require('./mkdir.js'),
-	mkFile : require('./mkFile.js')
+	mkFile : require('./mkFile.js'),
+	readFile : require('./readFile.js'),
+	readDir : require('./readDir.js')
 };
